c36cd61152fdfe5b63eb80fd13031e633dfce0a7
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-06-06 13:25:35 -0500

    added get and post request for animeController

--- controllers/animeController.js
+++ controllers/animeController.js
@@ -6,3 +6,9 @@ const router = express.Router()
 router.get('/', (req, res)=>{
     Anime.find().then(anime=>res.status(200).json({animes:anime}))
 })
+
+router.post('/', (req, res)=>{
+    Anime.create(req.body).then(anime=>res.status(200).json({animes:anime}))
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
+const animeController = require('./controllers/animeController')
 
 const PORT = process.env.PORT
 app.use(cors())
 app.use(express.json())
+
+app.use('/animes', animeController)
+
+
+
+
+
 
 
 app.listen(PORT, ()=>{
