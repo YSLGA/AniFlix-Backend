6bc87f65023e7103025804555072ae00758e0d9c
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-06-06 15:12:09 -0500

    added get and post requests for userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,3 +6,13 @@ const router = express.Router();
 router.get('/', (req,res)=>{
     User.find().then(user=>res.status(200).json({users:user}))
 })
+
+router.get('/:id', (req, res)=>{
+    User.findById(req.params.id).then(user=>res.status(200).json({users: user}))
+})
+
+router.post('/', (req, res)=>{
+    User.create(req.body).then(user=>res.json({users: user}))
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const animeController = require('./controllers/animeController')
+const userController = require('./controllers/userController')
 
 const PORT = process.env.PORT
 app.use(cors())
 app.use(express.json())
 
 app.use('/animes', animeController)
+app.use('/users', userController)
 
 
 
