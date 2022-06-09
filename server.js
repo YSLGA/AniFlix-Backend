fe1feced67bc4a4f209cb1c3433a7202bec94f3f
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-09 15:33:14 -0700

    added heroku info

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
-const PORT = process.env.PORT
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
 
-const animeController = require('./controllers/animeController')
-const animeMovieController = require('./controllers/animeMovieController')
+const animeController = require("./controllers/animeController");
+const animeMovieController = require("./controllers/animeMovieController");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/animes', animeController)
-app.use('/animeMovies', animeMovieController)
+app.use("/animes", animeController);
+app.use("/animeMovies", animeMovieController);
 
+app.set("port", process.env.PORT || 8080);
 
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
 
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log('we are live');
-} )
+app.listen(PORT, () => {
+    console.log("we are live");
+});
